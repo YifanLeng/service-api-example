5cdcb46c3809cf734f28a3e21f1fcef364203fb1
YifanLeng <yifanleng@brandeis.edu>
2020-02-27 00:38:44 -0500

    Ajax in the front end to avoid redirecting after clicking generate and delete buttons

--- frontend-sinatra/app.rb
+++ frontend-sinatra/app.rb
@@ -1,5 +1,6 @@
 # entry point
 require 'sinatra'
+require 'net/http'
 
 # retrieve your api host address like this
 #   settings.api => "someaddress.ondigitalocean.com"
@@ -8,5 +9,48 @@ set :api_port, ENV['API_PORT'] || 80
 
 # Show a pretty table of all users... rendered on a html page.
 get '/' do
-  erb :index
+  uri = URI.join("http://#{settings.api}:#{settings.api_port}", 
+  	             "/users")
+  res = Net::HTTP.get_response(uri)
+  @api_host = settings.api
+  @api_port = settings.api_port
+  case res
+  when Net::HTTPSuccess
+  	@users = JSON.parse(res.body)
+  	@count = @users.length
+  	erb :index
+  else
+  	erb :error_page
+  end
 end
+
+# the following routes are no longer needed because 
+# we use Ajax(XMLHttpRequest). Therefore, both of the 
+# post requests are send in the front end without re-routing
+# post '/users' do
+# 	uri = URI.join("http://#{settings.api}:#{settings.api_port}", 
+#   	             "/users")
+# 	puts "the count parameter is"
+# 	puts params.to_s
+# 	puts params[:count]
+# 	res = Net::HTTP.post_form(uri, 'n' => params[:count])
+# 	case res
+#   when Net::HTTPSuccess
+#   else
+#   	erb :error_page
+#   end
+# end
+
+# post '/users/destroy' do
+# 	uri = URI.join("http://#{settings.api}:#{settings.api_port}", 
+#   	             "/users/destroy")
+# 	res = Net::HTTP.post_form(uri, params)
+# 	case res
+#   when Net::HTTPSuccess
+#   else
+#   	erb :error_page
+#   end
+# end
+
+
+
--- service-api-sinatra/app.rb
+++ service-api-sinatra/app.rb
@@ -1,20 +1,65 @@
 # entry point
 require 'sinatra'
-
+require 'faker'
 require_relative './models/user'
+
+configure do
+  enable :cross_origin
+end  
+
+# later consider limiting Access-Control-Allow-Origin to only
+# the frontend-sinatra's url
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
 
 # Retrieve all users from database.
 get '/users' do
-  'Implement me!'
+  begin
+    users = []
+    User.all.each do |user|
+    	hash_object = {
+        :name => user.name, 
+        :email => user.email,
+        :bio => user.bio
+      }
+      users << hash_object
+    end
+    status 200
+    return users.to_json
+  rescue
+    status 500
+  end
 end
 
 # Create n users.
 post '/users' do
   count = params[:n].to_i || 1
-  'Implement me!'
+  if count > 30
+  	status 400
+  else
+	  count.times do
+	  	user = User.new
+	  	user.name = Faker::Name.name  
+	  	user.email = Faker::Internet.email
+	  	user.password = Faker::Internet.password
+	  	user.bio = Faker::Lorem.paragraph
+	  	# if saving to database fails 
+	  	# terminate returns 500 Internal error code
+	  	if !user.save
+	  		satus 500
+	  	end
+	  end
+	  status 200
+	 end
 end
 
 # Delete all users.
 post '/users/destroy' do
-  'Implement me!'
+  begin
+  	User.delete_all
+  	status 200
+  rescue
+  	status 500
+  end
 end
