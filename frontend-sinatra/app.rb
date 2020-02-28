75559ed7c96a2d7f8eba6b30ca4ed2347ddc13da
YifanLeng <yifanleng@brandeis.edu>
2020-02-27 19:20:20 -0500

    added horizontal bar

--- frontend-sinatra/app.rb
+++ frontend-sinatra/app.rb
@@ -10,17 +10,17 @@ set :api_port, ENV['API_PORT'] || 80
 # Show a pretty table of all users... rendered on a html page.
 get '/' do
   uri = URI.join("http://#{settings.api}:#{settings.api_port}", 
-  	             "/users")
+                 "/users")
   res = Net::HTTP.get_response(uri)
   @api_host = settings.api
   @api_port = settings.api_port
   case res
   when Net::HTTPSuccess
-  	@users = JSON.parse(res.body)
-  	@count = @users.length
-  	erb :index
+    @users = JSON.parse(res.body)
+    @count = @users.length
+    erb :index
   else
-  	erb :error_page
+    erb :error_page
   end
 end
 
@@ -28,27 +28,27 @@ end
 # we use Ajax(XMLHttpRequest). Therefore, both of the 
 # post requests are send in the front end without re-routing
 # post '/users' do
-# 	uri = URI.join("http://#{settings.api}:#{settings.api_port}", 
-#   	             "/users")
-# 	puts "the count parameter is"
-# 	puts params.to_s
-# 	puts params[:count]
-# 	res = Net::HTTP.post_form(uri, 'n' => params[:count])
-# 	case res
+#   uri = URI.join("http://#{settings.api}:#{settings.api_port}", 
+#                  "/users")
+#   puts "the count parameter is"
+#   puts params.to_s
+#   puts params[:count]
+#   res = Net::HTTP.post_form(uri, 'n' => params[:count])
+#   case res
 #   when Net::HTTPSuccess
 #   else
-#   	erb :error_page
+#     erb :error_page
 #   end
 # end
 
 # post '/users/destroy' do
-# 	uri = URI.join("http://#{settings.api}:#{settings.api_port}", 
-#   	             "/users/destroy")
-# 	res = Net::HTTP.post_form(uri, params)
-# 	case res
+#   uri = URI.join("http://#{settings.api}:#{settings.api_port}", 
+#                  "/users/destroy")
+#   res = Net::HTTP.post_form(uri, params)
+#   case res
 #   when Net::HTTPSuccess
 #   else
-#   	erb :error_page
+#     erb :error_page
 #   end
 # end
 
